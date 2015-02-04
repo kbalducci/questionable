75d733c25674f30936e54a08e9a3f9289a5f04b8
Kate <kbalducci@hotmail.com>
2015-02-03 21:21:41 -0600

    rolled back database, now includes id, fixed show controller q

--- db/migrate/20150203223911_create_answers.rb
+++ db/migrate/20150203223911_create_answers.rb
@@ -1,6 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
+      t.belongs_to :question, index:true
       t.text :ans_body
 
       t.timestamps null: false
